cc158877693ffdd9abaa26a23c23ec117ef056a3
qiangqiang.liu <qiangqiang.liu@tongdun.cn>
2018-07-06 17:13:50 +0800

    ttt

---
+++ src/main/java/com/lqq/App.java
@@ -0,0 +1,13 @@
+package com.lqq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
